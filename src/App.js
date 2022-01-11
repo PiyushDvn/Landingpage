9b1c335b04215e1df9b550f745d526d8fc7b22b7
Piyush <Piyush@SEZLAP-12916.india.rsystems.com>
2022-01-11 11:53:42 +0530

    landingpage deploy

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Landingpage from './Components/landingpage'
+
+function App() {
+  return (
+    <div className='Container'>
+      
+    <Landingpage />
+
+
+   </div>
+    
+  );
+}
+
+export default App;
---
+++ src/Components/landingpage.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Landingpage() {
+    return (
+
+            <div className="image1">
+                <div className="image2">
+                    <div className='leftheader'>
+                        <label>LANDED</label>
+                        <div className='rightheader'>
+                            <label>Home</label>
+                            <label>Layouts</label>
+                            <label>Elements</label>
+                            <a>Sign Up</a>
+                        </div></div>
+                    <body className='centertext'>
+                        <h1>The Future has landed</h1>
+                        <p> And there are no hoverboards or flying cars.<br>
+                        </br>
+                            Just apps. Lots fo mother flipping apps.</p>
+                        <img className='roundimage' src="dfg.png" alt="" />
+                    </body>
+                </div>
+            </div>
+
+    )
+}
+
+export default Landingpage
